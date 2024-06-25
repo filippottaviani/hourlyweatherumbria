74cfd74487294c14b512538be96ce6fd590279d9
filippottaviani <filippo.ottaviani.tr@gmail.com>
2024-06-25 16:29:02 +0200

    update per riservatezza

--- weather.py
+++ weather.py
@@ -1,7 +1,5 @@
 import requests
-
-api_key = "d288e2ebb6a9ec536d63d8233c18512c"
-base_url = "https://api.openweathermap.org/data/3.0/onecall?"
+import keys
 
 # Definisco le coordinate delle città
 terni = "TERNI", "42.58598160906223", "12.611514716492035"
@@ -13,7 +11,7 @@ norcia = "NORCIA", "42.79235444680194", "13.092800749063501"
 
 
 def current_weather(city):
-    url = base_url + "lat=" + city[1] + "&lon=" + city[2] + "&appid=" + api_key
+    url = "https://api.openweathermap.org/data/3.0/onecall?lat=" + city[1] + "&lon=" + city[2] + "&appid=" + keys.api_key
     response = requests.get(url).json()
 
     # Prelevo le info utili
@@ -21,11 +19,6 @@ def current_weather(city):
     humidity = response['current']['humidity']
     description = response['current']['weather'][0]['description']
 
-    # result = f"{city[0]}\n\tType: {description}\n\tTemperature: {temperature:.2f}°C\n\tHumidity: {humidity}%\n\n"
     result = f"#{city[0]}:\n{description}\n {temperature:.2f}°C\n {humidity}% H2O\n\n"
 
     return result
-
-
-# Prova
-# print(result)
